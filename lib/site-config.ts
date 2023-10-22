c9c84dd8bfb1c62cea32073ea176c11b816a4d2c
MahendraSH <maahi2001sh@gmail.com>
2023-10-22 20:48:43 +0530

    site config  og.png

--- lib/site-config.ts
+++ lib/site-config.ts
@@ -3,5 +3,5 @@ export const siteConfig = {
   description:
     "Portfolio Site of Mahendra, a FullStack Developer showcasing his projects and experiences",
   url: "https://mahendrash.vercel.app",
-  ogImage: "https://mahendrash.vercel.app/og.jpg",
+  ogImage: "https://mahendrash.vercel.app/og.png",
 };
