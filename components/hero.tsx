cc4f0323f51d27c9bb2322119a44678a7d06ecde
MahendraSH <maahi2001sh@gmail.com>
2024-03-19 11:15:12 +0530

    hero responsive

--- components/hero.tsx
+++ components/hero.tsx
@@ -5,8 +5,129 @@ import SocailLinks from "./ui/socail-links";
 import { Button } from "./ui/button";
 import Link from "next/link";
 import { LucideDownload } from "lucide-react";
-
+import { TypewriterEffect } from "@/components/ui/typewriter-effect";
 interface HeroProps {}
+const words = [
+  {
+    text: "'",
+    className: "text-blue-500 dark:text-blue-500",
+  },
+  {
+    text: "Computer",
+  },
+  {
+    text: "science",
+  },
+  {
+    text: "student",
+  },
+  {
+    text: "and",
+  },
+  {
+    text: "passionate",
+  },
+  {
+    text: "about",
+  },
+  {
+    text: "creating",
+  },
+  {
+    text: "responsive",
+  },
+  {
+    text: "and",
+  },
+  {
+    text: "user-friendly",
+  },
+  {
+    text: "web",
+  },
+  {
+    text: "applications",
+  },
+  {
+    text: "with",
+  },
+  {
+    text: "secure",
+  },
+  {
+    text: "backend",
+  },
+  {
+    text: "that",
+  },
+  {
+    text: "solve",
+  },
+  {
+    text: "real-world",
+  },
+  {
+    text: "problems.",
+  },
+  {
+    text: "Strong",
+  },
+  {
+    text: "problem-solving",
+  },
+  {
+    text: "skills",
+  },
+  {
+    text: "and",
+  },
+  {
+    text: "ability",
+  },
+  {
+    text: "to",
+  },
+  {
+    text: "work",
+  },
+  {
+    text: "collaboratively",
+  },
+  {
+    text: "in",
+  },
+  {
+    text: "a",
+  },
+  {
+    text: "team",
+  },
+  {
+    text: "environment.",
+  },
+  {
+    text: "And",
+  },
+  {
+    text: "passionate",
+  },
+  {
+    text: "about",
+  },
+  {
+    text: "learning",
+  },
+  {
+    text: "new",
+  },
+  {
+    text: "things.",
+  },
+  {
+    text: "'",
+    className: "text-blue-500 dark:text-blue-500",
+  },
+];
 
 const Hero: FC<HeroProps> = ({}) => {
   return (
@@ -14,10 +135,10 @@ const Hero: FC<HeroProps> = ({}) => {
       <div className="hero min-h-screen   bg-[url('/images.jpeg')] dark:bg-[url('/image.png')] ">
         <div className="hero-overlay  opacity-80  dark:opacity-40  "></div>
         <div className="hero-content flex-col lg:flex-row m-auto ">
-          <div className="card md:card-side  w-full h-full  shadow-md  shadow-muted-foregroundy glass      ">
+          <div className="card lg:card-side  w-full h-full  shadow-md  shadow-muted-foregroundy glass  lg:p-10 p-0      ">
             <figure>
               <div className=" flex-1  rounded-2xl ">
-                <div className=" md:w-80 sm:w-50  p-10 md:pr-12   max-h-90 ">
+                <div className=" lg:w-96 md:w-96  sm:w-50  lg:pr-12   max-h-90 ">
                   <Image
                     src="https://github.com/MahendraSH.png"
                     className=" rounded  "
@@ -28,23 +149,20 @@ const Hero: FC<HeroProps> = ({}) => {
                 </div>
               </div>
             </figure>
-            <div className="card-body  md:w-[calc(100%-320px)]   flex-1  ">
+            <div className="card-body  lg:w-[calc(100%-320px)]   flex-1  ">
               <h2 className=" md:text-5xl text-3xl font-bold bg-clip-text text-transparent  bg-gradient-to-tr from-slate-700  via-primary to-secondary-foreground dark:bg-gradient-to-tr dark:from-slate-300  dark:via-primary dark:to-secondary-foreground  ">
                 Mahendra S H
               </h2>
 
               <Separator />
 
-              <div className="flex flex-col gap-4 lg:pr-3 lg:mr-4 text-start ">
-                <p className="   md:text-lg lg:text-xl  ">
-                  "Computer science student and Passionate about creating
-                  responsive and user-friendly web applications with secure
-                  backend that solve real-world problems. Strong problem-solving
-                  skills and ability to work collaboratively in a team
-                  environment. and passionate about learning new things”
-                </p>
+              <div className="flex flex-col gap-4 lg:pr-3 lg:mr-4 text-start lg:min-w-[800px] min-w-full min-h-[200px] ">
+                <TypewriterEffect
+                  className="text-lg md:text-xl lg:text-2xl font-normal"
+                  words={words}
+                />
               </div>
-              <div className="card-actions  md:mt-3 mt-1  ">
+              <div className="card-actions  md:mt-3 pt-4  ">
                 <SocailLinks />
                 <Link
                   className=" ml-auto"
